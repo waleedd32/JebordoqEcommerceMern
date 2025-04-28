2fcc322c1dce1d0243d1980950ea017c9602eed2
waleedd32 <valid0@hotmail.com>
2025-04-28 23:50:10 +0300

    feat: setup additional routes for Collection, About, and Contact components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       App
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );

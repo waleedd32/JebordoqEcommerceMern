b8340dbd57843198f8412f154e2ab4f47d9505e9
waleedd32 <valid0@hotmail.com>
2025-05-03 19:14:56 +0300

    Add NavLink for Home in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>Home</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+      </ul>
     </div>
   );
 };

180b92aa2d7e2dae29a49b4e83ed4a7104c301cd
waleedd32 <valid0@hotmail.com>
2025-05-07 23:37:12 +0300

    Add profile dropdown menu with hover effect to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,6 +34,13 @@ const Navbar = () => {
             src={assets.profile_icon}
             alt=""
           />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded ">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>

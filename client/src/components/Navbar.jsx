2923c8ec52695f4a034149a6c726adddc3c9be69
waleedd32 <valid0@hotmail.com>
2025-05-08 20:29:48 +0300

    Add cart icon with item count badge to Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -42,6 +42,11 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+          {/* to show a red mark on the basket, use bg-red-500 */}
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]"></p>
+        </Link>
       </div>
     </div>
   );

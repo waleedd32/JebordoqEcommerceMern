e5da8d6c843d13bd22be83d693c66e2c47d987b5
waleedd32 <valid0@hotmail.com>
2025-05-16 00:31:04 +0300

    feat: add bestseller section to Hero with basic styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,7 +5,12 @@ const Hero = () => {
     <div className="flex flex-col sm:flex-row border border-gray-400">
       {/* Hero Left Side */}
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
-        Left side
+        <div className="text-[#414141]">
+          <div className="flex items-center gap-2">
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
+          </div>
+        </div>
       </div>
     </div>
   );

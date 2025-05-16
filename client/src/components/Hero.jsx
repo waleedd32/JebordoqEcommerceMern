15b3af1879461a91f058b0e642717f5f9ed7d7f1
waleedd32 <valid0@hotmail.com>
2025-05-17 00:42:51 +0300

    feat: update Hero with section headline, shop CTA, and right-side image

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
@@ -10,8 +11,18 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+            Latest Arrivals
+          </h1>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
+            <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
+          </div>
         </div>
       </div>
+
+      {/* Hero Right Side */}
+      <img className="w-full sm:w-1/2" src={assets.hero_img} alt="" />
     </div>
   );
 };

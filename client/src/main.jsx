5c8c398a0b308abfb95a70eaef80811b87ca357a
waleedd32 <valid0@hotmail.com>
2025-05-23 01:12:32 +0300

    Add LatestCollection component with basic product context integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>
 );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
 
 const Home = () => {
   return (
     <div>
       Home
       <Hero />
+      <LatestCollection />
     </div>
   );
 };

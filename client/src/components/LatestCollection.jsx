3f43af656b9ca2210f8027406b989b346bd89195
waleedd32 <valid0@hotmail.com>
2025-05-24 23:26:33 +0300

    Add Title component and update LatestCollection to use it

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -1,12 +1,19 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
 
   console.log(products);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Title text1="LATEST" text2="COLLECTIONS" />
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollection;

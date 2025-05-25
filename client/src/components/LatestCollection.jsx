aaf88a0c0114fad7361c6392ff4d3b01122ff863
waleedd32 <valid0@hotmail.com>
2025-05-25 23:48:16 +0300

    Enhance LatestCollection with description and styling

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -8,9 +8,14 @@ const LatestCollection = () => {
   console.log(products);
 
   return (
-    <div>
-      <div>
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
         <Title text1="LATEST" text2="COLLECTIONS" />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s.
+        </p>
       </div>
     </div>
   );

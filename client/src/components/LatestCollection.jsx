2853fcef21802164182e0959cf8ab495034ad0fd
waleedd32 <valid0@hotmail.com>
2025-05-27 23:49:47 +0300

    Create ProductItem component and render latest products

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
@@ -22,6 +23,18 @@ const LatestCollection = () => {
           since the 1500s.
         </p>
       </div>
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>{" "}
     </div>
   );
 };

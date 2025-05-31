6b78161dc79a78e5efd88b54faa4b051ba20bb19
waleedd32 <valid0@hotmail.com>
2025-05-31 23:55:48 +0300

    Add BestSeller component and mobile styles for active links

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       Home
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };

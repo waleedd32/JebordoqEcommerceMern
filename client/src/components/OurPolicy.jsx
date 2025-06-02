d3eaefc0b380f04f95922834f7f0664e3f3f3253
waleedd32 <valid0@hotmail.com>
2025-06-03 00:35:05 +0300

    Add OurPolicy component with exchange policy info

---
+++ client/src/components/OurPolicy.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const OurPolicy = () => {
+  return (
+    <div>
+      <div>
+        <img src={assets.exchange_icon} alt="" />
+        <p>Easy Exchange Policy</p>
+        <p>We offer hassle free exchange policy</p>
+      </div>
+    </div>
+  );
+};
+
+export default OurPolicy;

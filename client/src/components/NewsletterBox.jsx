9b9f1b1d0f31780ae11606338f97d17581e4ac75
waleedd32 <valid0@hotmail.com>
2025-06-07 00:02:11 +0300

    Add form submission handler and styling improvements to NewsletterBox

--- client/src/components/NewsletterBox.jsx
+++ client/src/components/NewsletterBox.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const NewsletterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="text-center">
       <p className="text-2xl font-medium text-gray-800">
@@ -10,18 +14,21 @@ const NewsletterBox = () => {
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry.
       </p>
-      <form>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3"
+      >
         <input
           className="w-full sm:flex-1 outline-none"
           type="email"
           placeholder="Enter your email"
-        />{" "}
+        />
         <button
           type="submit"
           className="bg-black text-white text-xs px-10 py-4"
         >
           SUBSCRIBE
-        </button>{" "}
+        </button>
       </form>
     </div>
   );

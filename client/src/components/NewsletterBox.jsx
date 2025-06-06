509a5f0fb57bd577909eb0e3935e1f0de2059163
waleedd32 <valid0@hotmail.com>
2025-06-06 23:57:43 +0300

    Enhance NewsletterBox with description, email input, and subscribe button

--- client/src/components/NewsletterBox.jsx
+++ client/src/components/NewsletterBox.jsx
@@ -6,6 +6,23 @@ const NewsletterBox = () => {
       <p className="text-2xl font-medium text-gray-800">
         Subscribe now & get 20% off
       </p>
+      <p className="text-gray-400 mt-3">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+      <form>
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email"
+        />{" "}
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4"
+        >
+          SUBSCRIBE
+        </button>{" "}
+      </form>
     </div>
   );
 };

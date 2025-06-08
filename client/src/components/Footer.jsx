e0c81b8352dc1ecf031992c9aad9c4f131b17424
waleedd32 <valid0@hotmail.com>
2025-06-09 02:55:34 +0300

    Style Footer with responsive grid layout and improved typography

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <div>
       {" "}
-      <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-          <img src={assets.logo} alt="" />
-          <p>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s.
@@ -16,8 +16,8 @@ const Footer = () => {
         </div>
 
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Delivery</li>
@@ -26,8 +26,8 @@ const Footer = () => {
         </div>
 
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
             <li>+1-212-456-7890</li>
             <li>contact@foreveryou.com</li>
           </ul>
@@ -35,7 +35,9 @@ const Footer = () => {
       </div>
       <div>
         <hr />
-        <p>Copyright 2024@ forever.com - All Rights Reserved</p>
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@ forever.com - All Rights Reserved
+        </p>
       </div>
     </div>
   );

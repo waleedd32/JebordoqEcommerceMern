f7a811a0fe677c564fc825177588d16d42da5d04
waleedd32 <valid0@hotmail.com>
2025-06-11 00:01:57 +0300

    Add category filter checkbox to Collection component

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -17,7 +17,12 @@ const Collection = () => {
             showFilter ? "" : "hidden"
           } sm:block `}
         >
-          {" "}
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={"Men"} /> Men
+            </p>
+          </div>
         </div>
       </div>
     </div>

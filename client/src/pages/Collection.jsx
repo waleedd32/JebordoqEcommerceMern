a15628238e6859437c9a99c3b9da8174a920f9ba
waleedd32 <valid0@hotmail.com>
2025-06-10 23:56:11 +0300

    Add basic filter UI layout to Collection component

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return <div>Collection</div>;
-};
+  const [showFilter, setShowFilter] = useState(false);
 
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter Options */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+        {/* Category Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block `}
+        >
+          {" "}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Collection;

b46f5f971ea59dac0d00ef7daf0ad9fd13f0abca
waleedd32 <valid0@hotmail.com>
2025-06-13 01:34:41 +0300

    Add multiple category filter options to Collection component

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -19,9 +19,15 @@ const Collection = () => {
         >
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-            <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Men"} /> Men
-            </p>
+            <div className="flex gap-2">
+              <input className="w-3" type="checkbox" value="Men" /> Men
+            </div>
+            <div className="flex gap-2">
+              <input className="w-3" type="checkbox" value="Women" /> Women
+            </div>
+            <div className="flex gap-2">
+              <input className="w-3" type="checkbox" value="Kids" /> Kids
+            </div>
           </div>
         </div>
       </div>

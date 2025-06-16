824ed3f0f65f95c79c51b43c9ef44561fd9334ec
waleedd32 <valid0@hotmail.com>
2025-06-17 01:51:51 +0300

    Add Title component and sorting dropdown to Collection page

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import Title from "../components/Title";
 
 const Collection = () => {
   const [showFilter, setShowFilter] = useState(false);
@@ -63,6 +64,18 @@ const Collection = () => {
           </div>
         </div>
       </div>
+      {/* Right Side */}
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title text1="ALL" text2="COLLECTIONS" />
+          {/* Product Sort */}
+          <select className="border-2 border-gray-300 text-sm px-2">
+            <option value="relevant">Sort by: Relevant</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+      </div>{" "}
     </div>
   );
 };

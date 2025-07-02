7a10140bbb794bb3a64313736738348ae15c7ae8
waleedd32 <valid0@hotmail.com>
2025-07-02 21:12:22 +0300

    Add ShopContext and SearchBar component for product search feature

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+
+const SearchBar = () => {
+  const { search, setSearch, showSearch, setShowSearch } =
+    useContext(ShopContext);
+
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;

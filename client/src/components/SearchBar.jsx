e0979e9aaaf81c118d9fa742bbf9541863932eef
waleedd32 <valid0@hotmail.com>
2025-07-03 22:37:50 +0300

    Update SearchBar component with input field and search icon UI

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
 
-  return <div>SearchBar</div>;
+  return showSearch ? (
+    <div>
+      <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="flex-1 outline-none bg-inherit text-sm"
+          placeholder="Search"
+        />
+        <img className="w-4" src={assets.search_icon} alt="" />
+      </div>
+    </div>
+  ) : null;
 };
 
 export default SearchBar;

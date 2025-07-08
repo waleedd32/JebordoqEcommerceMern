f1d4627a7b1e3e8a07d1c81dfc7c78c8c6b129bf
waleedd32 <valid0@hotmail.com>
2025-07-09 00:53:07 +0300

    Show SearchBar only on collection routes

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,12 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
+  const location = useLocation();
 
-  return showSearch ? (
+  useEffect(() => {
+    console.log("searchbar path name", location.pathname);
+    if (location.pathname.includes("collection")) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [location]);
+
+  return showSearch && visible ? (
     <div className="border-t border-b bg-gray-50 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input

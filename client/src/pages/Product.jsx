67a3f0d0d2fd567e4eda8dc474c09a8fcf5d62a5
waleedd32 <valid0@hotmail.com>
2025-07-18 01:06:23 +0300

    Refactor Product component to fetch product data using context and URL params

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+
+  console.log("productId:", productId);
+
+  return <div></div>;
 };
 
 export default Product;

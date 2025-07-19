a81f5964a9553fce79d24edc2f9c606a6dd8b778
waleedd32 <valid0@hotmail.com>
2025-07-19 22:59:50 +0300

    Fetch product data by ID from context in Product component

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,13 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
 
-  console.log("productId:", productId);
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        console.log("image: ", item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
 
   return <div></div>;
 };

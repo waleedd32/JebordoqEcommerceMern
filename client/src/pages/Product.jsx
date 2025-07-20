4c7c40ee7094f977f5db295895fc0075e74e3c94
waleedd32 <valid0@hotmail.com>
2025-07-20 23:53:10 +0300

    Display product images and update image state in Product component

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -6,12 +6,14 @@ const Product = () => {
   const { productId } = useParams();
   const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductData(item);
-        console.log("image: ", item.image[0]);
+        // console.log(item);
+        setImage(item.image[0]);
         return null;
       }
     });
@@ -21,7 +23,27 @@ const Product = () => {
     fetchProductData();
   }, [productId, products]);
 
-  return <div></div>;
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;

2d110369bb6feaaa66590993cc17c4304bc19555
waleedd32 <valid0@hotmail.com>
2025-07-23 20:32:59 +0300

    Add static star rating and review count to product details

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const Product = () => {
   const { productId } = useParams();
@@ -47,6 +48,14 @@ const Product = () => {
         {/* ------- Product Info --------- */}
         <div className="flex-1">
           <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-2">(122)</p>
+          </div>
         </div>
       </div>
     </div>

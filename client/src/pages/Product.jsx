1c5cb88076abecde6be31e56ba6f598452a7863c
waleedd32 <valid0@hotmail.com>
2025-07-25 15:53:38 +0300

    Display product price with currency and description

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -56,6 +56,13 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className="pl-2">(122)</p>
           </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
         </div>
       </div>
     </div>

1e7ccb5558add7525e4a25da85ccd1fb3521e665
waleedd32 <valid0@hotmail.com>
2025-08-04 00:08:25 +0300

    Add useEffect to filter related products by category and subCategory

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 const RelatedProducts = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
 
-  console.log("related products", products);
-  console.log("related category", category);
+  useEffect(() => {
+    if (products.length > 0) {
+      let productsCopy = products.slice();
+
+      productsCopy = productsCopy.filter((item) => category === item.category);
+      productsCopy = productsCopy.filter(
+        (item) => subCategory === item.subCategory
+      );
+
+      console.log("Filtered related products:", productsCopy.slice(0, 5));
+    }
+  }, [products]);
 
   return <div>RelatedProducts</div>;
 };

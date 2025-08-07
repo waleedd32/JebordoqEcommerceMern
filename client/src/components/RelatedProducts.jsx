c1bae2d210299f2c6f2ed6fee888876841f85a79
waleedd32 <valid0@hotmail.com>
2025-08-07 16:39:10 +0300

    Render filtered related products using ProductItem component

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import ProductItem from "./Productitem";
 import Title from "./Title";
 
 const RelatedProducts = ({ category, subCategory }) => {
@@ -23,6 +24,17 @@ const RelatedProducts = ({ category, subCategory }) => {
     <div className="my-24">
       <div className="text-center text-3xl py-2">
         <Title text1={"RELATED"} text2={"PRODUCTS"} />
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );

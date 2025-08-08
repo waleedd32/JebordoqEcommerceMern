e16396eda78af8e899db51dcd25c9247a35ed233
waleedd32 <valid0@hotmail.com>
2025-08-09 01:04:19 +0300

    feat: add cart functionality with addToCart and cartItems state

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -8,6 +8,28 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log("cart Items:", cartItems);
+  }, [cartItems]);
 
   const value = {
     products,
@@ -17,6 +39,8 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
   };
 
   return (

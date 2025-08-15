025e3860384fa825d181acc63df1ccdbe179de98
waleedd32 <valid0@hotmail.com>
2025-08-15 16:22:41 +0300

    generate cartData from products and cartItems in Cart component

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Cart = () => {
+  const { products, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      const tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item],
+            });
+          }
+        }
+      }
+      console.log("tempData:", tempData);
+    }
+  }, [cartItems, products]);
   return <div>Cart</div>;
 };
 

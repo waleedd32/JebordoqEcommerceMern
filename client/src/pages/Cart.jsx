38bb36b2edbd2796e44704df08f6bf813d9022a3
waleedd32 <valid0@hotmail.com>
2025-08-17 19:17:32 +0300

    update Cart component to render cart items with product image

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Cart = () => {
   const { products, cartItems } = useContext(ShopContext);
@@ -20,9 +21,39 @@ const Cart = () => {
         }
       }
       console.log("tempData:", tempData);
+      setCartData(tempData);
     }
   }, [cartItems, products]);
-  return <div>Cart</div>;
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1="YOUR" text2="CART" />
+      </div>
+
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  className="w-16 sm:w-20"
+                  src={productData.image[0]}
+                  alt=""
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;

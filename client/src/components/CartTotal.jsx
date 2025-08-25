546072a2bb7455a308c6c8b753b74bf049e16691
waleedd32 <valid0@hotmail.com>
2025-08-25 13:17:10 +0300

    feat: add CartTotal component with subtotal display using ShopContext

--- client/src/components/CartTotal.jsx
+++ client/src/components/CartTotal.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const CartTotal = () => {
-  return <div>CartTotal</div>;
+  const { currency, getCartAmount } = useContext(ShopContext);
+
+  return (
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={"CART"} text2={"TOTALS"} />
+      </div>
+
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>
+            {currency}
+            {getCartAmount()}.00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
   const { products, currency, cartItems, updateQuantity } =
@@ -89,6 +90,19 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          <CartTotal />
+          <div className="w-full text-end">
+            <button
+              onClick={() => navigate("/place-order")}
+              className="bg-black text-white text-sm my-8 px-8 py-3"
+            >
+              PROCEED TO CHECKOUT
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );

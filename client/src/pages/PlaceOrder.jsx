a0b39c3d40bced8b8d9176fdd280e764ae13cc28
waleedd32 <valid0@hotmail.com>
2025-08-30 01:19:06 +0300

    feat: add delivery information section with Title component to PlaceOrder

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* ------------- Left Side ------------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceOrder;

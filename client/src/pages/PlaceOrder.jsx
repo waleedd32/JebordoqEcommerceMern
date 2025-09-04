fbace80f208ae63fe9c8c4d816d5d7f2ae71e444
waleedd32 <valid0@hotmail.com>
2025-09-04 23:34:11 +0300

    feat: add payment method selection UI with Stripe option

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 import Title from "../components/Title";
 
 const PlaceOrder = () => {
+  const [method, setMethod] = useState("cod");
+
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       {/* ------------- Left Side ------------- */}
@@ -82,6 +86,25 @@ const PlaceOrder = () => {
       <div className="mt-8 min-w-80">
         <div className="mt-8">
           <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text1={"PAYMENT"} text2={"METHOD"} />
+          {/* Payment Method Selection */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+            <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "stripe" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                className="h-5 mx-4"
+                src={assets.stripe_logo}
+                alt="Stripe Logo"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>

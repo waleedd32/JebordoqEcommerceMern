713313f1e361584367830d142094a508170c88b0
waleedd32 <valid0@hotmail.com>
2025-09-05 00:42:33 +0300

    update PlaceOrder component: add COD option and setup Razorpay (commented)

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -104,6 +104,34 @@ const PlaceOrder = () => {
                 alt="Stripe Logo"
               />
             </div>
+            {/* <div
+              onClick={() => setMethod("razorpay")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "razorpay" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <img
+                className="h-5 mx-4"
+                src={assets.razorpay_logo}
+                alt="Stripe Logo"
+              />
+            </div> */}
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
+            </div>
           </div>
         </div>
       </div>

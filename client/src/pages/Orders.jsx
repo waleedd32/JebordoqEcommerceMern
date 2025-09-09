cd1e3c1dadbc952e594f12eb05e6ea763e3234d6
waleedd32 <valid0@hotmail.com>
2025-09-09 23:50:31 +0300

    feat: add Orders page with ShopContext and Title component

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products, currency } = useContext(ShopContext);
+
+  return (
+    <div className="border-t pt-16">
+      <div className="text-2xl">
+        <Title text1={"MY"} text2={"ORDERS"} />
+      </div>
+    </div>
+  );
 };
 
 export default Orders;

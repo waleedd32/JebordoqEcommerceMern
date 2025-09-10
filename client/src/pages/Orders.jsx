ac26273e7fe6a828a317a1271c651da713f4603b
waleedd32 <valid0@hotmail.com>
2025-09-11 00:25:45 +0300

    feat: display product list in Orders page

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -10,6 +10,19 @@ const Orders = () => {
       <div className="text-2xl">
         <Title text1={"MY"} text2={"ORDERS"} />
       </div>
+
+      <div>
+        {products.slice(1, 4).map((item, index) => (
+          <div
+            key={index}
+            className="py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+          >
+            <div className="flex items-start gap-6 text-sm">
+              <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

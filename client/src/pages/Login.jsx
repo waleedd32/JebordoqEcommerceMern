fb5bc35eb0005415a91702218800dc24dee6fcaa
waleedd32 <valid0@hotmail.com>
2025-09-24 00:23:34 +0300

    Show name input only when not in Login state

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,19 +2,24 @@ import React, { useState } from "react";
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("Login");
+
   return (
     <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
       </div>
-      <input
-        type="text"
-        className="w-full px-3 py-2 border border-gray-800"
-        placeholder="Name"
-        required
-      />
 
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+        />
+      )}
       <input
         type="email"
         className="w-full px-3 py-2 border border-gray-800"

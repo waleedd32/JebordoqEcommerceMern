d12826ec1f669d24bcff723f102470cbdc713b25
waleedd32 <valid0@hotmail.com>
2025-09-30 11:56:32 +0300

    Add Title component to About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Title from "../components/Title";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+    </div>
+  );
 };
 
 export default About;

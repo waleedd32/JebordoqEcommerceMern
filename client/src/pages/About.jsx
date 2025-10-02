0bfc8b14a8771b5c9b6f829d03a4d6af9b53e317
waleedd32 <valid0@hotmail.com>
2025-10-02 23:46:06 +0300

    Add about page image using assets

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
     <div>
       <div className="text-2xl text-center pt-8 border-t">
         <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
       </div>
     </div>
   );

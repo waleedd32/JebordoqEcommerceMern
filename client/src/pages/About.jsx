cabbe4cfc36567d2c9e5a0dc4398b0ac469939b9
waleedd32 <valid0@hotmail.com>
2025-10-04 01:04:13 +0300

    Add 'Why Choose Us' section to About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -38,6 +38,18 @@ const About = () => {
           </p>
         </div>
       </div>
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            We thoroughly select and evaluate each product to ensure it meets
+            our high standards of quality.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

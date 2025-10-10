f03921c1e812779654cac0e7acffea4e9af9cf8b
waleedd32 <valid0@hotmail.com>
2025-10-11 01:46:15 +0300

    Add Contact page with Title component and contact image

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      <div>
+        <img src={assets.contact_img} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;

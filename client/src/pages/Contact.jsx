0ed1ad94ccf0a22e9b3d8ff0a869f08d136058ac
waleedd32 <valid0@hotmail.com>
2025-10-13 00:07:17 +0300

    Enhance Contact page with centered title, flex layout, and responsive image styling

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,11 +5,15 @@ import { assets } from "../assets/assets";
 const Contact = () => {
   return (
     <div>
-      <div>
+      <div className="text-center text-2xl pt-10 border-t">
         <Title text1={"CONTACT"} text2={"US"} />
       </div>
-      <div>
-        <img src={assets.contact_img} alt="" />
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
       </div>
     </div>
   );

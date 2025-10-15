c7b816e969ae334ff040ca3d871a74db4ff398de
waleedd32 <valid0@hotmail.com>
2025-10-16 00:34:59 +0300

    Add careers section, Join Our Team button, and NewsletterBox to Contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Contact = () => {
   return (
@@ -22,8 +23,19 @@ const Contact = () => {
           <p className="text-gray-500">
             Tel: (415) 555-0132 <br /> Email: admin@forever.com
           </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Join Our Team
+          </button>
         </div>
       </div>
+
+      <NewsletterBox />
     </div>
   );
 };

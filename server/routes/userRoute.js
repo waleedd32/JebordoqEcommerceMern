2346d49a8b0a4b18cb24ecb481d867153c774dd4
waleedd32 <valid0@hotmail.com>
2025-10-30 00:29:44 +0200

    feat(auth): implement user login with JWT and bcrypt

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -11,6 +11,29 @@ const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
+userRouter.post("/login", async (req, res) => {
+  console.log("JWT Secret:", process.env.JWT_SECRET);
+
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+});
+
 userRouter.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;

8319be4f9689e718f96f986f7c4037546499e0af
waleedd32 <valid0@hotmail.com>
2025-11-02 01:10:04 +0200

    Add product details and image logging in product add route

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -14,6 +14,41 @@ productRouter.post(
   ]),
   async (req, res) => {
     try {
+      const {
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller,
+      } = req.body;
+
+      const image1 = req.files.image1 && req.files.image1[0];
+      const image2 = req.files.image2 && req.files.image2[0];
+      const image3 = req.files.image3 && req.files.image3[0];
+      const image4 = req.files.image4 && req.files.image4[0];
+      console.log(
+        "body: ",
+        name,
+        description,
+        price,
+        category,
+        subCategory,
+        sizes,
+        bestseller
+      );
+
+      console.log(
+        "image1: ",
+        image1,
+        "image2: ",
+        image2,
+        "image3: ",
+        image3,
+        "image4. ",
+        image4
+      );
       res.json({ success: true, message: "Product Added" });
     } catch (error) {
       console.log(error);

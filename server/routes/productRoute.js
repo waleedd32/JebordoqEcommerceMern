fcf2817dad4d84e6e6327f34ac6d8c252f404dff
waleedd32 <valid0@hotmail.com>
2025-11-02 23:46:02 +0200

    Integrate Cloudinary image upload for product addition

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import upload from "../middleware/multer.js";
+import { v2 as cloudinary } from "cloudinary";
 
 const productRouter = express.Router();
 
@@ -28,6 +29,20 @@ productRouter.post(
       const image2 = req.files.image2 && req.files.image2[0];
       const image3 = req.files.image3 && req.files.image3[0];
       const image4 = req.files.image4 && req.files.image4[0];
+
+      const images = [image1, image2, image3, image4].filter(
+        (item) => item !== undefined
+      );
+
+      let imagesUrl = await Promise.all(
+        images.map(async (item) => {
+          let result = await cloudinary.uploader.upload(item.path, {
+            resource_type: "image",
+          });
+          return result.secure_url;
+        })
+      );
+
       console.log(
         "body: ",
         name,
@@ -39,16 +54,7 @@ productRouter.post(
         bestseller
       );
 
-      console.log(
-        "image1: ",
-        image1,
-        "image2: ",
-        image2,
-        "image3: ",
-        image3,
-        "image4. ",
-        image4
-      );
+      console.log("imagesUrl", imagesUrl);
       res.json({ success: true, message: "Product Added" });
     } catch (error) {
       console.log(error);

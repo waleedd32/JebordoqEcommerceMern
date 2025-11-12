325776276fc2473fda001d8bad3048701a9e872b
waleedd32 <valid0@hotmail.com>
2025-11-12 16:00:34 +0200

    Implement /list route for fetching all products

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -68,4 +68,15 @@ productRouter.post(
   }
 );
 
+// function for list product
+productRouter.get("/list", async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+});
+
 export default productRouter;

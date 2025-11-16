4166b6c363c19cbd6d99429fe2447231026c1647
waleedd32 <valid0@hotmail.com>
2025-11-16 23:24:10 +0200

    Add admin login route and JWT token generation

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -80,4 +80,24 @@ userRouter.post("/register", async (req, res) => {
   }
 });
 
+// Route for admin login
+userRouter.post("/admin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+});
+
 export default userRouter;

34c98c0f9eafb93c20aa37e55beb952466a0a727
waleedd32 <valid0@hotmail.com>
2025-11-25 23:51:55 +0200

    Add Sidebar component and update App layout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div>
-      Apps
-      <Navbar />
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr />
+        <div className="flex w-full">
+          <Sidebar />
+        </div>
+      </>
     </div>
   );
 };

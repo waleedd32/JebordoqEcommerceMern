066a0f41a926f3207e54e5abf6253b35c5e1591b
waleedd32 <valid0@hotmail.com>
2025-11-26 01:07:57 +0200

    Add NavLink and icon to sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div>
+        <NavLink to="/add">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

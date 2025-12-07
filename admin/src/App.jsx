820cc369213c6be8fab3001f2d41afa17c099caa
waleedd32 <valid0@hotmail.com>
2025-12-08 00:58:14 +0200

    Add Routes setup in App and render Add component at /add

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
 
 const App = () => {
   return (
@@ -10,6 +12,11 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add />} />
+            </Routes>
+          </div>
         </div>
       </>
     </div>

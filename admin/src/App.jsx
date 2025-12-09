7736a4d5228f3621cf1b7489529a2c1f9d1e241d
waleedd32 <valid0@hotmail.com>
2025-12-09 21:21:44 +0200

    Add routes for List and Orders components in App

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add";
-
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 const App = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
@@ -14,7 +15,11 @@ const App = () => {
           <Sidebar />
           <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
             <Routes>
+              <Route path="/" element={<Add />} />
+
               <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
             </Routes>
           </div>
         </div>

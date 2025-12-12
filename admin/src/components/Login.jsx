155da98f91d85152f5398a1df50d36e1a868619b
waleedd32 <valid0@hotmail.com>
2025-12-13 00:51:40 +0200

    Implement initial layout and fields for Login component

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div>
+        <h1>Admin Panel</h1>
+
+        <form>
+          <div>
+            <p>Email Address</p>
+            <input type="email" placeholder="your@email.com" required />
+          </div>
+          <div>
+            <p>Password</p>
+            <input type="password" placeholder="Enter your password" required />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

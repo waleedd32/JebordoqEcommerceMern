87355e64cd00ad252fe61ef371f9b885422a369a
waleedd32 <valid0@hotmail.com>
2025-12-16 00:21:17 +0200

    Enhance Login component with styled inputs and login button

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Login = () => {
+const Login = ({ setToken }) => {
   return (
     <div className="min-h-screen flex items-center justify-center w-full">
       <div className="bg-white shadow-md rounded-lg px-8 py-6 max-w-md">
@@ -11,12 +11,28 @@ const Login = () => {
             <p className="text-sm font-medium text-gray-700 mb-2">
               Email Address
             </p>
-            <input type="email" placeholder="your@email.com" required />
+            <input
+              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
+              type="email"
+              placeholder="your@email.com"
+              required
+            />
           </div>
-          <div>
-            <p>Password</p>
-            <input type="password" placeholder="Enter your password" required />
+          <div className="mb-3 min-w-72">
+            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
+            <input
+              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
           </div>
+          <button
+            className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
+            type="submit"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>

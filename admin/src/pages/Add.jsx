5b2f31ed96664882383702f3fcb017f0bf34350b
waleedd32 <valid0@hotmail.com>
2025-12-26 02:08:58 +0200

    Add image upload section in Add component

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Add = () => {
-  return <div>Add</div>;
+  return (
+    <form>
+      <div>
+        <p>Upload Image</p>
+
+        <div>
+          <label htmlFor="">
+            <img src={assets.upload_area} alt="" />
+          </label>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Add;

a1d65d73f8b19ace91e473cba48d8ecc6445c525
waleedd32 <valid0@hotmail.com>
2025-12-26 02:04:08 +0200

    feat(navbar): clear  token on logout click

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = ({ setToken }) => {
         src={assets.logo}
         alt=""
       />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
         Logout
       </button>
     </div>

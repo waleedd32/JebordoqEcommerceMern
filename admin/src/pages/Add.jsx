1cc1972b62e6182957ee794717f26722c3cf1561
waleedd32 <valid0@hotmail.com>
2026-01-04 00:51:13 +0200

    Add upload placeholders and product name input to Add form

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
-const Add = () => {
+const Add = ({ token }) => {
   return (
-    <form>
+    <form className="flex flex-col w-full items-start gap-3">
       <div>
-        <p>Upload Image</p>
-
-        <div>
+        <p className="mb-2">Upload Image</p>
+        <div className="flex gap-2">
           <label htmlFor="image1">
-            <img className="w-20" alt="" />
+            <img className="w-20" src={assets.upload_area} alt="" />
             <input type="file" id="image1" hidden />
           </label>
           <label htmlFor="image2">
-            <img className="w-20" alt="" />
+            <img className="w-20" src={assets.upload_area} alt="" />
             <input type="file" id="image2" hidden />
           </label>
           <label htmlFor="image3">
-            <img className="w-20" alt="" />
+            <img className="w-20" src={assets.upload_area} alt="" />
             <input type="file" id="image3" hidden />
           </label>
           <label htmlFor="image4">
-            <img className="w-20" alt="" />
+            <img className="w-20" src={assets.upload_area} alt="" />
             <input type="file" id="image4" hidden />
           </label>
         </div>
+      </div>
+      <div>
+        <p className="mb-2">Product name</p>
+        <input type="text" placeholder="Type here" required />
       </div>
     </form>
   );

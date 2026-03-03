72708639dc4408e93d73e6aca2d562b6847a5588
waleedd32 <valid0@hotmail.com>
2026-03-03 23:58:06 +0200

    Add product list fetching logic to List component using axios and useEffect

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
 
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      console.log(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return <div>List</div>;
 };
 

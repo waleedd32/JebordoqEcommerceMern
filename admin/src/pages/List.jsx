a240685c073aa6a09f29400b7416a5dcac4b00aa
waleedd32 <valid0@hotmail.com>
2026-03-13 01:00:05 +0200

    Add responsive product list table layout to List component with Tailwind grid styling

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -29,15 +29,18 @@ const List = ({ token }) => {
     <div>
       {" "}
       <p className="mb-2">All Products List</p>
-      <div>
-        {/* ------- List Table Title -------- */}
-        <div>
+      <div className="flex flex-col gap-2">
+        {/* ------- List Table Title --------- */}
+
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
           <b>Image</b>
           <b>Name</b>
           <b>Category</b>
           <b>Price</b>
-          <b>Action</b>
+          <b className="text-center">Action</b>
         </div>
+
+        {/* ------- Product List ------- */}
       </div>
     </div>
   );

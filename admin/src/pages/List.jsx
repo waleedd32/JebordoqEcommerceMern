230e5a990eee48cacc15b169738db2e0137ee945
waleedd32 <valid0@hotmail.com>
2026-03-14 23:59:28 +0200

    Add product removal functionality and render product list with delete action in List component

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -21,6 +21,26 @@ const List = ({ token }) => {
     }
   };
 
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/product/remove",
+        { id },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchList();
   }, []);
@@ -41,6 +61,27 @@ const List = ({ token }) => {
         </div>
 
         {/* ------- Product List ------- */}
+
+        {list.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );

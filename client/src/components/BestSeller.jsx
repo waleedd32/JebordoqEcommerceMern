f141883a32e8eb73938920691d446f3f96a59691
waleedd32 <valid0@hotmail.com>
2026-03-24 23:28:45 +0200

    fix: resolve ProductItem casing issue

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
-import ProductItem from "./ProductItem";
+import ProductItem from "./Productitem";
 
 const BestSeller = () => {
   const { products } = useContext(ShopContext);
